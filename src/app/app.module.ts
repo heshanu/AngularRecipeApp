a51347204d92aaed18161710b5dfa120a8bfc779
heshanu <heshanu97@gmail.com>
2022-04-05 12:42:53 -0700

    x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './compoent/header/header.component';
+import { RecipesComponent } from './compoent/recipes/recipes.component';
+import { RecipeDetailComponent } from './compoent/recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './compoent/recipes/recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './compoent/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './compoent/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesItemComponent } from './compoent/recipes/recipe-list/recipes-item/recipes-item.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    ShoppingEditComponent,
+    ShoppingListComponent,RecipesItemComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/compoent/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'a test recipe',
+      'this is simple recipe',
+      'https://cdn.pixabay.com/photo/2018/07/18/19/12/pasta-3547078_960_720.jpg'
+    ),
+    new Recipe(
+      'Another test recipe',
+      'this is simple recipe',
+      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg'
+    ),
+    new Recipe(
+      'a test recipe',
+      'this is simple recipe',
+      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg'
+    ),
+    new Recipe(
+      'a test recipe',
+      'this is simple recipe',
+      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg'
+    ),
+    new Recipe(
+      'a test recipe',
+      'this is simple recipe',
+      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg'
+    ),
+    new Recipe(
+      'a test recipe',
+      'this is simple recipe',
+      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg'
+    ),
+    new Recipe(
+      'a test recipe',
+      'this is simple recipe',
+      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg'
+    ),
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
