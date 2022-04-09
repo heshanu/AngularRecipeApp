fe8047519257a457a2550442e4d9c4573079bec9
heshanu <heshanu97@gmail.com>
2022-04-09 10:45:52 -0700

    added new features

--- src/app/compoent/recipes/recipe-service.service.ts
+++ src/app/compoent/recipes/recipe-service.service.ts
@@ -1,54 +1,60 @@
 import { EventEmitter, Injectable, Output } from '@angular/core';
+import { Ingredient } from 'src/app/Shared/Ingredient.model';
 import { Recipe } from './recipes.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeServiceService {
-  recipeSelected=new EventEmitter<Recipe >();
+  recipeSelected = new EventEmitter<Recipe>();
   //@Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'a test recipe',
       'this is simple recipe',
-      'https://cdn.pixabay.com/photo/2018/07/18/19/12/pasta-3547078_960_720.jpg'
+      'https://cdn.pixabay.com/photo/2018/07/18/19/12/pasta-3547078_960_720.jpg',
+      [new Ingredient('meat', 1), new Ingredient('Rice', 20)]
     ),
     new Recipe(
       'Another test recipe',
       'this is simple recipe',
-      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg'
+      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg',
+      [new Ingredient('fish', 10), new Ingredient('Rice Fried', 20)]
     ),
     new Recipe(
       'a test recipe',
       'this is simple recipe',
-      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg'
+      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg',
+      [new Ingredient('meat', 100), new Ingredient('Rice  smaba', 20)]
     ),
     new Recipe(
       'a test recipe',
       'this is simple recipe',
-      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg'
+      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg',
+      [new Ingredient('pork meat', 1), new Ingredient('Rice pork', 20)]
     ),
     new Recipe(
       'a test recipe',
       'this is simple recipe',
-      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg'
+      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg',
+      []
     ),
     new Recipe(
       'a test recipe',
       'this is simple recipe',
-      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg'
+      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg',
+      []
     ),
     new Recipe(
       'a test recipe',
       'this is simple recipe',
-      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg'
+      'https://www.wellplated.com/wp-content/uploads/2017/11/Cinnamon-Roasted-Butternut-Squash-Easy-butternut-squash-recipe.jpg',
+      []
     ),
   ];
-  constructor() { }
+  constructor() {}
 
-  getRecipes(){
+  getRecipes() {
     return this.recipes.slice();
   }
-
-
 }
